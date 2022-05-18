35576638f557dfa920604b524b7448eee086c627
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2022-05-18 17:03:42 -0400

    removed testt file

--- test.py
+++
@@ -1,7 +0,0 @@
-from pip._vendor import requests as r
-
-request = r.get('https://foxes90-prempundit.herokuapp.com/players')
-players_api = request.json()
-print(players_api['Players'][0]['first_name'])
-
-
