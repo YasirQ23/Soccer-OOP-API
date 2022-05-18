2474a36c934692e7b34808b3e5e93d970729d71c
Yasir Qureshi <yasirqureshi@Yasirs-Air.attlocal.net>
2022-05-18 19:50:57 -0400

    add input base

--- project.py
+++ project.py
@@ -1,48 +1,38 @@
 from pip._vendor import requests as r
 
+
 class Team:
     def __init__(self, team_name):
         self.name = team_name
         self.formation = ''
-        self.available_pos = [1, 1, 1, 1]
+        self.available_pos = [0, 0, 0, 0]
         self.fieldability = True
-        self.roster = []
-
-    def valid_lineup(self):
-        if sum(self.available_pos) < 11:
-            self.fieldability = False
-        elif self.available_pos[0] == 0:
-            self.fieldability = False
-        elif self.available_pos[1] < 3 or self.available_pos[1] > 5:
-            self.fieldability = False
-        elif self.available_pos[2] < 3 or self.available_pos[2] > 5:
-            self.fieldability = False
-        elif self.available_pos[3] < 1 or self.available_pos[3] > 3:
-            self.fieldability = False
-    
-#    def pos_assignment(self):
+        self.active_roster = []
 
 
 class Player:
-    def __init__(self, name, position,team, playable):
+    def __init__(self, name, position, team, playability):
         self.name = name
         self.position = position
         self.team = team
-        self.playable = None
+        self.playability = playability
+
 
 class Run:
     def api_request(self):
-        self.request = r.get('https://foxes90-prempundit.herokuapp.com/players')
+        self.request = r.get(
+            'https://foxes90-prempundit.herokuapp.com/players')
         self.players_api = self.request.json()
-    
+
     def team_creation(self):
+        self.teams = {}
         x = set()
         for i in (range(len(self.players_api['Players']))):
             x.add(self.players_api['Players'][i]['team'])
         x = list(x)
         for i in x:
-            i = Team(i)
-            
+            k = Team(i)
+            self.teams[i] = k
 
     def player_creation(self):
         for i in (range(len(self.players_api['Players']))):
@@ -53,20 +43,56 @@ class Run:
             playability = True
             if (self.players_api['Players'][i]['injured']) == True or (self.players_api['Players'][i]['suspended']) == True:
                 playability = False
-            last_name = Player(f'{first_name} {last_name}',pos_,team_name,playability)
-#            if last_name.playability == True:
-#                append
-    
+            last_name = Player(f'{first_name} {last_name}',
+                               pos_, team_name, playability)
+            if last_name.playability == True:
+                self.teams[last_name.team].active_roster.append(last_name)
 
+    def player_position(self):
+        for i in self.teams:
+            for k in range(len(self.teams[i].active_roster)):
+                x = self.teams[i].active_roster[k].position
+                if x == 'Keeper':
+                    self.teams[i].available_pos[0] += 1
+                elif x == 'Defender':
+                    self.teams[i].available_pos[1] += 1
+                elif x == 'Midfielder':
+                    self.teams[i].available_pos[2] += 1
+                elif x == 'Striker':
+                    self.teams[i].available_pos[3] += 1
 
-    
+    def valid_lineup(self):
+        for i in self.teams:
+            if sum(self.teams[i].available_pos) < 11:
+                self.teams[i].fieldability = False
+            elif self.teams[i].available_pos[0] == 0:
+                self.teams[i].fieldability = False
+            elif self.teams[i].available_pos[1] < 3 or self.teams[i].available_pos[1] > 5:
+                self.teams[i].fieldability = False
+            elif self.teams[i].available_pos[2] < 3 or self.teams[i].available_pos[2] > 5:
+                self.teams[i].fieldability = False
+            elif self.teams[i].available_pos[3] < 1 or self.teams[i].available_pos[3] > 3:
+                self.teams[i].fieldability = False
 
+    def whosyourteam(self):
+        x = input(
+            'Tell me your team, Ill tell you if they can field for their upcoming game\n').title()
+        team_name = set()
+        for i in self.teams:
+            team_name.add(i)
+        if x in team_name:
+            if self.teams[x].fieldability == False:
+                print(f'Sorry bud {x} is not going to be able to play')
+            else:
+                print(f'Lucky you {x} will be able to play.')
+        else:
+            print(f'Input "{x}" Is Invalid')
 
 
 run_ = Run()
 run_.api_request()
 run_.team_creation()
 run_.player_creation()
-print(Aston.__dict__)
-
-
+run_.player_position()
+run_.valid_lineup()
+run_.whosyourteam()
